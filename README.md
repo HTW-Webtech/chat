1b0b0a64638ef00b8535f6176e6ad4b4053d4da9
Max Beier <beierm@htw-berlin.de>
2019-05-29 15:47:15 +0200

    refinement

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # chat
+
 Einfacher Chat basierend auf Node.js und Express.js
+
+## Benutzung
+
+```sh
+npm install   # installiert benötigte Pakete
+npm start     # startet Server
+npm run dev   # startet Entwicklungsserver
+```
+
+## Debugging
+
+1. install [ndb](https://github.com/GoogleChromeLabs/ndb#readme) (`npm install -g ndb`)
+2. run `ndb npm run dev`
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+const { join } = require('path');
 const express = require('express');
+const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const app = express();
 
 app.set('view engine', 'ejs'); // views sind vom typ "ejs"
-app.set('views', __dirname + '/views'); // … und liegen im ordner "views"
-app.use(express.static(__dirname + '/public')); // alles im ordner "public" soll direkt als datei ausgeliefert werden
+app.set('views', join(__dirname, '/views')); // … und liegen im ordner "views"
+app.use(express.static(join(__dirname, '/public'))); // alles im ordner "public" soll direkt als datei ausgeliefert werden
+app.use(helmet()); // gibt mehr sicherheit
 app.use(cookieParser()); // cookies sollen vorberarbeitet werden
 app.use(bodyParser.urlencoded({ extended: false })); // … und formularinhalte auch
 
@@ -78,4 +81,9 @@ app.get('/logout', (req, res) => {
   res.redirect('/login');
 });
 
-app.listen(5000);
+const port = process.env.PORT || 5000;
+
+app.listen(port, err => {
+  if (err) throw err;
+  console.log(`Server is running on http://localhost:${port}`);
+});
